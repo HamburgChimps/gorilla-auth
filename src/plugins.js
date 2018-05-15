4f53dd1979241e63c5394930f66d03f1e0eaced6
Jacob Evans <jakegod14@gmail.com>
2018-05-15 21:57:39 +0200

    Add plugins as peerDep

--- src/plugins.js
+++ src/plugins.js
@@ -1,46 +1,23 @@
 const pjson = require('../package.json')
-const { spawn } = require('child_process')
 const pino = require('pino')
 
 class Plugins {
   constructor () {
     this._log = pino()
-    this._plugins = pjson.gorillaPlugins
+    this._plugins = pjson.peerDependencies
   }
 
   async load () {
     if (!this._plugins) return this._log.info('No plugins loaded')
-    await this._install()
-    return Object.keys(this._plugins).map(url => require(url))
-  }
-
-  _getUrls () {
-    let result = []
-    for (let key in this._plugins) {
-      result.push(this._plugins[key])
-    }
-    return result
-  }
-
-  async _install () {
-    for (let url of this._getUrls()) {
-      await new Promise((resolve, reject) => {
-        this._log.info(`Installing ${url}`)
-        const npm = spawn('npm', ['install', url, '--no-save'])
-        npm.stdout.on('data', (data) => {
-          this._log.info(`stdout: ${data}`)
-        })
-        npm.on('close', (code) => {
-          if (code > 0) {
-            this._log.error(`Error process exited with code ${code}`)
-            reject()
-          } else {
-            this._log.info(`child process exited with code ${code}`)
-            resolve()
-          }
-        })
-      })
-    }
+    return Object.keys(this._plugins).map(plugin => {
+      try {
+        const plugin = require(plugin)
+        return plugin
+      } catch (err) {
+        this._log.error(`Error not able to load plugin ${plugin}`)
+        throw err
+      }
+    })
   }
 }
 
